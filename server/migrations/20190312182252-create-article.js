95162d1852aab76e2790a518451af1c1fc044ccc
P.C. Ndayisenga <paccy@Pacifiques-MacBook-Pro.local>
2019-03-12 20:47:06 +0200

    [feature-164516483] Create article model and migration

---
+++ server/migrations/20190312182252-create-article.js
@@ -0,0 +1,35 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('articles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      body: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      tags: {
+        type: Sequelize.ARRAY(Sequelize.STRING)
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('articles');
+  }
+};
