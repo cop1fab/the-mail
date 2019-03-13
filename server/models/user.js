d234fae654fc9e7542530e47f25b994c3a64bc45
Copain Fabrice  BIENAIME <copainfabricebienaime@Dr-Copain.local>
2019-03-13 11:01:32 +0200

    [feature#164516445] adds user models using sequelize

--- server/models/user.js
+++ server/models/user.js
@@ -1,1 +1,12 @@
-console.log("user model");
+    'use strict';
+module.exports = (sequelize, DataType) => {
+    const User = sequelize.define('Article', {
+        firstName: DataTypes.STRING,
+        lastName: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+    });
+    User.associate = function(models){
+        
+    }
+}
