dab722aaff92f5e01cb47861478d9341306532d6
blaisebakundukize <blaisebakundukize@gmail.com>
2019-03-14 12:54:39 +0200

    Feature #164535146 Refactor comment model remove comments in code

--- server/models/comment.js
+++ server/models/comment.js
@@ -1,38 +1,37 @@
-// 'use strict';
-// export default (sequelize, DataTypes) => {
-//   const Comment = sequelize.define('comment', {
-//     body: {
-//       type: DataTypes.STRING,
-//       allowNull: false
-//     },
-//     id_article: {
-//       type: DataTypes.INTEGER,
-//       references: {
-//         model: 'Article',
-//         key: 'id',
-//         as: 'id_article'
-//       }
-//     },
-//     id_user: {
-//       type: DataTypes.INTEGER,
-//       references: {
-//         model: 'User',
-//         key: 'id',
-//         as: 'id_user'
-//       }
-//     }
-//   }, {});
-//   Comment.associate = (models) => {
-//     // associations can be defined here
-//     Comment.belongsTo(models.User, {
-//       foreignKey: 'userId',
-//       onDelete: 'CASCADE'
-//     });
+export default (sequelize, DataTypes) => {
+  const Comment = sequelize.define('comment', {
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    id_article: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Article',
+        key: 'id',
+        as: 'id_article'
+      }
+    },
+    id_user: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'User',
+        key: 'id',
+        as: 'id_user'
+      }
+    }
+  }, {});
+  Comment.associate = (models) => {
+    // associations can be defined here
+    Comment.belongsTo(models.User, {
+      foreignKey: 'userId',
+      onDelete: 'CASCADE'
+    });
 
-//     Comment.belongsTo(models.Article, {
-//       foreignKey: 'articleId',
-//       onDelete: 'CASCADE'
-//     });
-//   };
-//   return Comment;
-// };
+    Comment.belongsTo(models.Article, {
+      foreignKey: 'articleId',
+      onDelete: 'CASCADE'
+    });
+  };
+  return Comment;
+};
