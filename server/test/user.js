6012cfb2e998121491cb8b271576806de6bffcc8
Copain Fabrice  BIENAIME <copainfabricebienaime@Dr-Copain.local>
2019-03-14 14:20:57 +0200

    [chore#164624937] added user tests

--- server/test/user.js
+++ server/test/user.js
@@ -1,1 +1,47 @@
-console.log("user test");
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../app';
+import { endianness } from 'os';
+
+const {
+    expect
+  } = chai;
+
+chai.use(chaiHttp);
+
+describe('POST /api/v1/auth/signup', () =>{
+    it('should return the data of the created user', (done) =>{
+        chai.request(app)
+        .post('/api/v1/auth/signup')
+        .send({
+            firstName: 'Copain',
+            lastName: 'Fabrice',
+            email: 'bienaime.fabrice@andela.com',
+            password: '2423K!'
+        })
+        .end((err, res) =>{
+            expect(req.body.firstName).to.equal('Copain');
+            expect(req.body.lastName).to.equal('Fabrice');
+            expect(req.body.email).to.equal('bienaime.fabrice@andela.com');
+            done();
+        })
+    })
+});
+
+describe('login ',() =>{
+    describe('POST /api/v1/auth/login', () =>{
+        it('it should return the user information if the user exists', () =>{
+            chai.request(chai)
+            .post('/api/v1/auth login')
+            .send({
+                email: 'bienaime.fabrice@andela.com',
+                password: '2423K!'
+            })
+            .end((err, res) =>{
+                expect(res.status).to.equal(200);
+                expect(Object.keys(res.body.data).legnth).to.be.above(0);
+                (done);
+            })
+        })
+    })
+});
